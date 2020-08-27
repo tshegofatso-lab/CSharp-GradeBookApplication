21df5bd41d85d18c01a17d3af781ef88f44beeb1
tshegofatso-lab <tsfela@gmail.com>
2020-08-27 14:46:25 +0200

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-      BaseGradeBook gradeBook;
+      BaseGradeBook gradeBook = new BaseGradeBook(name);
       if (parts[2]=="standard")
       {
         gradeBook = new StandardGradeBook(parts[2]);
