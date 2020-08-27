819d9a270d601938b6f222d5907cd5b61046ff1f
tshegofatso-lab <tsfela@gmail.com>
2020-08-27 13:37:55 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,25 @@ namespace GradeBook.GradeBooks
 		public RankedGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Ranked;
+		}
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count<5)
+			{
+				throw new InvalidOperationException();
+			}
+			averageGrade = averageGrade / Students.Count;
+			if (averageGrade >= 80)
+				return 'A';
+			else if (averageGrade >= 60)
+				return 'B';
+			else if (averageGrade >= 40)
+				return 'C';
+			else if (averageGrade >= 20)
+				return 'D';
+			else
+				return 'F';
+
 		}
 	}
 }
